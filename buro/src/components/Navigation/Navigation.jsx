1fc20ffd5d8cefe6999108c3318740c63542f5df
Julia Romanenko <rja-97@yandex.ru>
2019-03-12 19:59:48 +0300

    created navigation

--- buro/src/components/Navigation/Navigation.jsx
+++ buro/src/components/Navigation/Navigation.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import './Navigation.css';
 
 function Navigation() {
-    return <div>
-
+    return <div className='menu'>
+        <ul className='menu__list'>
+            <li className='menu__list_item'>Мода</li>
+            <li className='menu__list_item'>Культура</li>
+            <li className='menu__list_item'>Красота</li>
+            <li className='menu__list_item'>Технологии</li>
+            <li className='menu__list_item'>Личность</li>
+            <li className='menu__list_item'>Видео</li>
+        </ul>
     </div>
 }
 
