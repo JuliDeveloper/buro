80bc8d8d8b840842bb523a76e8cebcc4779ceae0
Julia Romanenko <rja-97@yandex.ru>
2019-03-14 20:55:52 +0300

    added components

---
+++ buro/src/components/Block_projects/Block.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Block.css';
+import Project from '../Project/Project.jsx';
+
+function Block() {
+    return <div>
+        <Project />
+    </div>
+}
+
+export default Block;
