371b701c201a1fa8282fa9c784497b5cdfd0d0be
Julia Romanenko <rja-97@yandex.ru>
2019-03-14 22:11:08 +0300

    made a block with projects

--- buro/src/components/Block_projects/Block.jsx
+++ buro/src/components/Block_projects/Block.jsx
@@ -3,8 +3,9 @@ import './Block.css';
 import Project from '../Project/Project.jsx';
 
 function Block() {
-    return <div>
-        <Project />
+    return <div className='block'>
+        <Project title='Russian project' text='За кого проголосуете?' image='image probleski_port' link='Голосовать &raquo;' block='block__project background__grey' margin_css='margin' />
+        <Project title='Russian project' text='Выбираем лучшие проекты, созданные в 2018 году россиянами. 6 номинаций — 6 лауреатов.' image='image awards_2018' block='block__project background__white' />
     </div>
 }
 
