b7bc1932944a24f6a6db48bebdb805cdbaecaaad
Julia Romanenko <rja-97@yandex.ru>
2019-03-15 17:31:15 +0300

    created footer

--- buro/src/App.jsx
+++ buro/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 import './style.css';
 import './App.css';
+import Footer from '../src/components/Footer/Footer.jsx';
 
 function App() {
   return <Fragment>
-
+    <Footer />
   </Fragment>
 }
 
---
+++ buro/src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Footer.css';
+import List from '../List/List.jsx';
+
+function Footer() {
+    return <div className='footer'>
+        <div className='footer__box'>
+            <div className='footer__up'>
+                <span className='footer__up_arrow'></span>
+                <a className='footer__up_link' href="#">Вернуться наверх</a>
+            </div>
+        </div>
+        <div className='footer__lists'>
+            <List title='Контакты' item1='Поиск' item2='Просмотры' item3='Комментарии' item4='Прочее' />
+            <List title='О проекте' item1='Авторы' item2='Подписчики' item3='Медиа' item4='Агенты' item5='Инструкции' item6='Вакансии' border__css='list__border' />
+            <List title='Подписки' item1='Подписка' item2='Новости' item3='Создать аккаунт' item4='Личный кабинет' border__css='list__border' />
+            <List title='Архив статей' item1='Искусство' item2='Кино' item3='Музыка' item4='Театр' item5='Мода' item6='Поп-культура' border__css='list__border' />
+            <List title='Ресурсы' item1='Свяжитесь с нами' item2='Продукт' item3='FAQs' item4='Помощь' border__css='list__border' />
+            <List title='Подпишитесь на нас' item1='Facebook' item2='Twitter' item3='Instagram' item4='YouTube' border__css='list__border' />
+        </div>
+    </div>
+}
+
+export default Footer;
