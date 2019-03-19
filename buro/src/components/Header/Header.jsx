c7f5473125e8873e91cecbc8197460b2588dbe3e
Julia Romanenko <rja-97@yandex.ru>
2019-03-19 20:43:01 +0300

    added tablet version of header

--- buro/src/components/Header/Header.jsx
+++ buro/src/components/Header/Header.jsx
@@ -5,10 +5,19 @@ import Logo from '../Logo/Logo.jsx';
 import Navigation from '../Navigation/Navigation.jsx';
 
 function Header() {
-    return <div>
-        <Authorization />
-        <Logo />
-        <Navigation />
+    return <div className='header'>
+        <div className='header__desktop'>
+            <Authorization />
+            <Logo />
+            <Navigation />
+        </div>
+        <div className='header__tablet'>
+            <Logo />
+            <div className='header__tablet_box'>
+                <Authorization className='authorization' />
+                <Navigation />
+            </div>
+        </div>
     </div>
 }
 
